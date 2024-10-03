61543f66bfa65da2fab09e1cb5dfa04682ceee76
seungjjun <il1uso@naver.com>
2024-10-03 12:58:37 +0900

    #noissue feat project setting

---
+++ src/main/java/com/ddd/ravit/RavitApplication.java
@@ -0,0 +1,13 @@
+package com.ddd.ravit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RavitApplication.class, args);
+    }
+
+}
